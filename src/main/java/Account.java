9cc8fd5ddb8cb94c1eab159310e5d65b3cd3207a
Anthony <adbeal@ucdavis.edu>
2021-11-14 23:54:38 -0800

    updated accounts

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,28 +1,40 @@
 public enum Account {
-    CORPORATE("800000 Corporate", 800000),
-    CHECKING("800001 Checking", 800001);
+    admin_CORPORATE("800000 Corporate", "admin"),
+    admin_CHECKING("800001 Checking", "admin"),
+    jsmith_SAVINGS("800002 Savings", "jsmith"),
+    jsmith_CHECKING("800003 Checking", "jsmith"),
+    jsmith_CREDIT_CARD("4539082039396288 Credit Card", "jsmith");
     private final String name;
-    private final int id;
+    private final String user;
 
     public String getName() {
         return this.name;
     }
-
-    public int getId() {
-        return this.id;
+    public String getId() {
+        return this.name.split(" ")[0];
     }
 
-    Account(String name, int id) {
+    public String getUser() {
+        return this.user;
+    }
+
+    Account(String name, String user) {
         this.name = name;
-        this.id = id;
+        this.user = user;
     }
 
     public static Account selectAccount(String name) {
         switch (name) {
-            case "CHECKING":
-                return Account.CHECKING;
-            case "CORPORATE":
-                return Account.CORPORATE;
+            case "admin_CORPORATE":
+                return Account.admin_CORPORATE;
+            case "admin_CHECKING":
+                return Account.admin_CHECKING;
+            case "jsmith_SAVINGS":
+                return Account.jsmith_SAVINGS;
+            case "jsmith_CHECKING":
+                return Account.jsmith_CHECKING;
+            case "jsmith_CREDIT_CARD":
+                return Account.jsmith_CREDIT_CARD;
         }
         throw new IllegalArgumentException();
     }
